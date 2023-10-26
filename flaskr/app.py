c936849e03322e4b06d093dd750cda4beb7cffc0
ParkByeongJu <bj4906@gmail.com>
2023-10-26 17:46:10 +0900

    빨리...추가 기능...

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,26 +1,34 @@
-from flask import Flask, url_for, render_template, request, redirect, session
-import cx_Oracle as db
+from flask import Flask, render_template, Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from io import BytesIO
 
 app = Flask(__name__)
-app.secret_key="secret_key"
-
-def db():
-    dsn = db.makedsn('localhost', 1521, 'xe')
-    con = db.connect("hr", "hr", dsn)
-    cursor = con.cursor()
-    return cursor
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def create_plot():
+    connection = cx_Oracle.connect(user='hr', password='hr', dsn='localhost:1521/xe')
+    cursor = connection.cursor()
+    cursor.execute("SELECT age, COUNT(*) FROM detected_faces GROUP BY age")
+    data = cursor.fetchall()
+    cursor.close()
+    connection.close()
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.bar(ages, counts)
+    ax.set_xlabel('Age')
+    ax.set_ylabel('Count')
+    ax.set_title('Number of detected faces by age')
+    
+    # x축 정렬
+    plt.xticks(ages, rotation=45)
 
-@app.route('/LoginProcess')
-def loginProcess():
-    pass
+    # 그래프를 이미지 파일로 저장하지 않고 BytesIO 객체에 저장합니다.
+    output = BytesIO()
+    FigureCanvas(fig).print_png(output)
+
+    return Response(output.getvalue(), mimetype='image/png')
+
+
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(debug=True)
